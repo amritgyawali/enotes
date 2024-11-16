8e84f2accae161dec620dbdbe7481703947757f5
Amrit Gyawali <amritgyawali999@gmail.com>
2024-11-16 22:46:04 +0545

    update category implementation

--- src/main/java/com/amrit/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/amrit/service/impl/CategoryServiceImpl.java
@@ -32,15 +32,35 @@ public class CategoryServiceImpl implements CategoryService {
 //        category.setIsActive(categoryDto.getIsActive());
 
         Category category = mapper.map(categoryDto, Category.class);
-        category.setIsDeleted(false);
-        category.setCreatedBy(1);
-        category.setCreatedOn(new Date());
+
+        if (ObjectUtils.isEmpty(category.getId())){
+            category.setIsDeleted(false);
+            category.setCreatedBy(1);
+            category.setCreatedOn(new Date());
+        }
+        else{
+            updateCategory(category);
+        }
+
         Category saveCategory = categoryRepo.save(category);
         if(ObjectUtils.isEmpty(saveCategory))
         {
             return false;
         }
         return true;
+    }
+
+    private void updateCategory(Category category) {
+        Optional<Category> findById = categoryRepo.findById(category.getId());
+        if(findById.isPresent()){
+            Category existCategory = findById.get();
+            category.setCreatedBy(existCategory.getCreatedBy());
+            category.setCreatedOn(existCategory.getCreatedOn());
+            category.setIsDeleted(existCategory.getIsDeleted());
+
+            category.setUpdatedBy(1);
+            category.setUpdatedOn(new Date());
+        }
     }
 
     @Override
