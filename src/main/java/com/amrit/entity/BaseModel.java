983612bca328c55514e9e8d4266994f38f854642
Amrit Gyawali <amritgyawali999@gmail.com>
2024-12-16 23:15:35 +0545

    before auditing category

--- src/main/java/com/amrit/entity/BaseModel.java
+++ src/main/java/com/amrit/entity/BaseModel.java
@@ -1,20 +1,34 @@
 package com.amrit.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.util.Date;
 
 @Getter
 @Setter
 @MappedSuperclass
-public class BaseModel {
+public abstract class BaseModel {
+	
+	@CreatedBy
+	@Column(updatable = false)
+	private Integer createdBy;
 
-    private Boolean isActive;
-    private Boolean isDeleted;
-    private Integer createdBy;
-    private Date createdOn;
-    private Integer updatedBy;
-    private Date updatedOn;
+	@CreatedDate
+	@Column(updatable = false)
+	private Date createdOn;
+
+	@LastModifiedBy
+	@Column(insertable = false)
+	private Integer updatedBy;
+
+	@LastModifiedDate
+	@Column(insertable = false)
+	private Date updatedOn;
 }
